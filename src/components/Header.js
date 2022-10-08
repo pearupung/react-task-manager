a2f9f2f02b428ea6c6eb1c63e7b134e8350bde5e
Pearu Pung <pungpearu@gmail.com>
2022-10-08 20:45:21 +0300

    #2 Style inline for dynamic styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,19 @@ import PropTypes from 'prop-types'
 const Header = ({title}) => {
   return (
     <div>
-      <h1>{title}</h1>
+      <h1 style= {{color: 'red', backgroundColor: 'black'}}>
+          {title}
+        </h1>
     </div>
   )
 }
 
+Header.defaultProps = {
+    title: "Task Tracker",
+}
+
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
+    title: PropTypes.string,
 }
 
 export default Header
