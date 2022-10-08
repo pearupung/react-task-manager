00b515f98435d29bde637a79a42e94460e15eb6b
Pearu Pung <pungpearu@gmail.com>
2022-10-08 21:01:09 +0300

    #2 Add CSS and an add button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types'
 
-const style = { color: 'red', backgroundColor: 'black' }
 const Header = ({title}) => {
   return (
-    <div>
-      <h1 style= {style}>
-          {title}
-        </h1>
+    <div className='header'>
+        {title}
+      <button className='btn'>
+        Add
+      </button>
     </div>
   )
 }
