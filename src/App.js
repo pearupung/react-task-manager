3e4a7979f839fc425186db95db71bb6f9c714ee6
Pearu Pung <pungpearu@gmail.com>
2022-10-09 12:30:16 +0300

    #2 Implement reminder toggling

--- src/App.js
+++ src/App.js
@@ -8,37 +8,39 @@ function App() {
         id: 1,
         text: "Bring the knight a shruberry",
         day: '2022-12-03',
-        reminder: '2022-12-01',
+        reminder: false,
     },
     {
         id: 2,
         text: "Wake up in the morning",
         day: '2022-11-03',
-        reminder: '2022-11-01',
+        reminder: false,
     },
     {
         id: 3,
         text: "Clean your room",
         day: '2022-10-03',
-        reminder: '2022-10-01',
+        reminder: false,
     },
 ])
 
 
   const deleteTask = (id) => {
-    console.log(id)
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
   const toggleReminder = (id) => {
-    console.log(id)
+    setTasks(tasks.map((task) => task.id===id ? 
+      {...task, reminder: !task.reminder} :
+      task  
+    ))
   }
 
 
   return (
     <div className='container'>
       <Header ></Header>
-      {tasks.length == 0 ? 
+      {tasks.length === 0 ? 
         'No tasks to show.' :
         <Tasks 
           tasks={tasks} 
