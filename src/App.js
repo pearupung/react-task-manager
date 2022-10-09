c704a91edb3ac2aca088be939f40563a47b0ea0a
Pearu Pung <pungpearu@gmail.com>
2022-10-09 11:50:57 +0300

    #2 Pass ttasks through App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import Header from "./components/Header"
 import Tasks from './components/Tasks'
+import { useState } from 'react'
+
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: "Bring the knight a shruberry",
+        day: '2022-12-03',
+        reminder: '2022-12-01',
+    },
+    {
+        id: 2,
+        text: "Wake up in the morning",
+        day: '2022-11-03',
+        reminder: '2022-11-01',
+    },
+    {
+        id: 3,
+        text: "Clean your room",
+        day: '2022-10-03',
+        reminder: '2022-10-01',
+    },
+])
+
+
   return (
     <div className='container'>
       <Header ></Header>
-      <Tasks></Tasks>
+      <Tasks tasks={tasks}></Tasks>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,28 +1,5 @@
-import { useState } from 'react'
-
-
-const Tasks = () => {
-  const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: "Bring the knight a shruberry",
-        day: '2022-12-03',
-        reminder: '2022-12-01',
-    },
-    {
-        id: 2,
-        text: "Wake up in the morning",
-        day: '2022-11-03',
-        reminder: '2022-11-01',
-    },
-    {
-        id: 3,
-        text: "Clean your room",
-        day: '2022-10-03',
-        reminder: '2022-10-01',
-    },
-])
-
+const Tasks = ({ tasks }) => {
+  
   return (
     tasks.map(task => (<h3 key={task.id}>
         {task.text}
